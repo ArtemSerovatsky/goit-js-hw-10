56b8ce68e42489ba1725fda3b5147523e1cdf959
Artem-s-79 <a.serovatsky@gmail.com>
2020-11-19 18:17:54 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import './styles.css';
+import menuJson from './menu.json';
+import itemsTamplate from './template/menu.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const inputRef = document.querySelector('.theme-switch__toggle');
+inputRef.addEventListener('change', handleChange);
+
+const localStorageTheme = localStorage.getItem('theme');
+
+function handleChange() {
+  if (!inputRef.checked) {
+    addLightTheme();
+  } else {
+    addDarkTheme();
+  }
+}
+
+function addLightTheme() {
+  document.body.classList.remove(Theme.DARK);
+  document.body.classList.add(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function addDarkTheme() {
+  document.body.classList.remove(Theme.LIGHT);
+  document.body.classList.add(Theme.DARK);
+  localStorage.setItem('theme', Theme.DARK);
+}
+
+function lookLocalStorage() {
+  if (localStorageTheme === Theme.DARK) {
+    document.body.classList.toggle(Theme.DARK);
+    inputRef.checked = true;
+  }
+}
+
+lookLocalStorage();
+
+const markup = itemsTamplate(menuJson);
+
+const galleryRef = document.querySelector('.js-menu');
+galleryRef.insertAdjacentHTML('beforeend', markup);
